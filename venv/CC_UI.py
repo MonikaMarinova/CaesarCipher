4672a221a7cad68869f106a49f73adec5126b46e
Monika <monikasmarinova@gmail.com>
2018-12-30 22:11:11 +0200

    solution

---
+++ venv/CC_UI.py
@@ -0,0 +1,28 @@
+from tkinter import  *
+from CC_main_func import *
+
+
+# creating a window
+root = Tk()
+
+# creating and displaying a layout using frames
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side = BOTTOM)
+
+# creating and displaying info label
+hello_CC_label = Label(topFrame, text = " Hi, this is Caesar's Cipher" )
+hello_CC_label.pack(side = LEFT)
+text_lbl = Label(topFrame, text = "Insert text here: ")
+text_lbl.pack(side = LEFT)
+key_lbl = Label (topFrame, text = "Insert key between 1 and 25")
+key_lbl.pack(side = LEFT)
+
+#creating and displaying buttons
+encryption_button = Button(bottomFrame, text = "Encrypt")
+encryption_button.pack(side = RIGHT)
+decryption_button = Button(bottomFrame, text = "Decrypt")
+decryption_button.pack(side = LEFT)
+
+root.mainloop()
